dcac883e48081e271f3e120d56a8eeeece6c8679
David Fachini <david.fachini@gmail.com>
2021-09-22 14:10:05 -0300

    Test Iron Connect 2

--- dags/iron-test.py
+++ dags/iron-test.py
@@ -9,17 +9,17 @@ from datetime import timedelta
 
 # [START Query MongoDB Data Collection Produtos]
 # def query_mongo_collection():
-    import pymongo
-    import json
-    import pandas as pd
-    from pandas.io.json import json_normalize
+import pymongo
+import json
+import pandas as pd
+from pandas.io.json import json_normalize
 
 # [START MongoDB Connector]
     # client = pymongo.MongoClient('mongodb://root:VQLnZB1QIp@mongodb.airflow.svc.cluster.local:27017')
-    from airflow.hooks.base_hook import BaseHook
-    conn = BaseHook.get_connection('iron_analytics_db')
-    # db = conn.
-    print(conn)
+from airflow.hooks.base_hook import BaseHook
+conn = BaseHook.get_connection('iron_analytics_db')
+# db = conn.
+print(conn)
 # [END MongoDB Connector]
 
 

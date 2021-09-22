1f592ca082c65c5e9654b8fe6f857327c3038933
David Fachini <david.fachini@gmail.com>
2021-09-22 18:02:36 -0300

    Teste Mentoria 4

--- dags/teste-orlando.py
+++ dags/teste-orlando.py
@@ -53,7 +53,7 @@ default_args = {
 
 # [START instantiate_dag]
 with DAG(
-    'mongodb_dag_orlando',
+    'mongodb_dag',
     default_args=default_args,
     description='Query and Export MongoDB Data',
     schedule_interval=timedelta(days=1),
